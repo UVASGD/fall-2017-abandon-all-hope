61c7250f2d07c15fcca9b85aee14ca7b9486ffb3
Ryan McCampbell <rmccampbell7@gmail.com>
2017-02-19 12:58:45 -0500

    Created basic platformer

---
+++ Abandon All Hope/Assets/Scripts/KeyboardMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour {
+
+    public float speed = 10;
+    public float jumpspeed = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rigidbody2D body = GetComponent<Rigidbody2D>();
+		if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            body.AddForce(Vector2.left * speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            body.AddForce(Vector2.right * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            body.AddForce(Vector2.up * jumpspeed);
+        }
+	}
+}
