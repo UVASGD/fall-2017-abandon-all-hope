2700fa06058493cc70ef2f045ff64ba7c524496c
Wliu <Wliu1402@gmail.com>
2017-02-26 13:36:18 -0500

    Bullets!

---
+++ Abandon All Hope/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+
+	public Vector2 velocity;
+	private double timeout = 5.0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += (Vector3) velocity;
+		timeout -= Time.deltaTime;
+		if (timeout <= 0) {
+			Destroy(this.gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		// TODO: Check tag
+		Destroy (this.gameObject);
+	}
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		// TODO: Check tag
+		//Destroy (this.gameObject);
+		print("hi");
+	}
+}
--- Abandon All Hope/Assets/Scripts/KeyboardMovement.cs
+++ Abandon All Hope/Assets/Scripts/KeyboardMovement.cs
@@ -7,7 +7,11 @@ public class KeyboardMovement : MonoBehaviour {
     public float speed = 10;
     public float jumpspeed = 100;
 
+	public BulletMovement bullet;
+	public float bulletSpeed = 0.5f;
+
     private bool grounded = false;
+	private int facingRight = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -22,15 +26,22 @@ public class KeyboardMovement : MonoBehaviour {
 		if (Input.GetKey(KeyCode.LeftArrow))
         {
             body.AddForce(Vector2.left * speed);
+			facingRight = -1;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             body.AddForce(Vector2.right * speed);
+			facingRight = 1;
         }
         if (grounded && Input.GetKeyDown(KeyCode.UpArrow))
         {
             body.AddForce(Vector2.up * jumpspeed);
         }
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			BulletMovement bullet2 = Instantiate(bullet, transform.position + new Vector3(.7f * facingRight, .1f), Quaternion.identity);
+			bullet2.velocity = new Vector2(bulletSpeed * facingRight, 0);
+		}	
 	}
 
     private void CheckGrounded()
