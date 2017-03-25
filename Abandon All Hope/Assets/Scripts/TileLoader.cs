161fc7883ccb8cf1249f7aaf16f77e27fb82e977
Ryan McCampbell <rmccampbell7@gmail.com>
2017-03-25 01:37:39 -0400

    tile maps

---
+++ Abandon All Hope/Assets/Scripts/TileLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileLoader : MonoBehaviour
+{
+
+    public TextAsset mapFile;
+    public GameObject platform;
+
+    // Use this for initialization
+    void Start()
+    {
+        using (StringReader reader = new StringReader(mapFile.text))
+        {
+            string line;
+            int y = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] vals = line.Split(',');
+                for (int x = 0; x < vals.Length; x++)
+                {
+                    int type = int.Parse(vals[x]);
+                    if (type != 19)
+                        Instantiate(platform, new Vector2(x, -y), Quaternion.identity, transform);
+                }
+                y++;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
