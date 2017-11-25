c71a556c24c79bd8eee0775aa318bf629e8853c0
Julian <jaykmcclinton@gmail.com>
2017-11-25 08:22:00 -0500

    Rocks and Stuff
    Don't update to this intermediate commit
    
    - added modular rock platforms and detail
    - modified movement to match scale of artwork
    - longer song loop
    - organized art files
    - animations and stuff
    - tweaked shooting mechanic to match firing locations

--- Abandon All Hope/Assets/Scripts/LD_Parallax.cs
+++ Abandon All Hope/Assets/Scripts/LD_Parallax.cs
@@ -1,21 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class LD_Parallax : MonoBehaviour {
-    public float スピード = 1;
 
-    private Vector3 スタート;
-    private float レン;
+    private Vector2 レン;
+    private Vector3 スタート, ビLウ;
+    private Camera シ;
+    private SpriteRenderer 写真;
 
+    //スタティックのデータを設定します
     void Start() {
         スタート = transform.position;
+        シ = GameObject.Find("Main Camera").GetComponent<Camera>();
         GameObject 左 = GameObject.Find("hidari");
         GameObject 右 = GameObject.Find("migi");
-        レン = 右.transform.position.x - 左.transform.position.x;
+        GameObject ue = GameObject.Find("ue");
+        GameObject shita = GameObject.Find("shita");
+        写真 = GetComponent<SpriteRenderer>();
+        レン = new Vector2(右.transform.position.x - 左.transform.position.x,
+            ue.transform.position.y - shita.transform.position.y);
+        ビLウ = レン - 写真.sprite.rect.size;
     }
 
+    //写真はプレーヤーに移動します
     void Update() {
-        float ポス = Mathf.Repeat(Time.time * スピード, レン);
-        transform.position = スタート + Vector3.forward * ポス;
+        Vector3 カム = シ.gameObject.transform.position;
+        Vector2 シLウ = レン - シ.pixelRect.size, レ = new Vector2 (シLウ.x/ ビLウ.x, シLウ.y / ビLウ.y);
+        Debug.Log("L: "+レン+ "\tR: "+レ);
+        transform.position = new Vector3(レ.x * カム.x, レ.y * カム.y, 0)+ スタート;
     }
 }
